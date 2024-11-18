f22187ef19249fa38943a5bd905e50a98aba75c3
Ives M. W <ivesmw@gmail.com>
2024-11-18 17:53:02 -0300

    feat: implement GetCategory use case

---
+++ catalog-admin-api/src/category/application/get-category/get-category.usecase.int.spec.ts
@@ -0,0 +1,43 @@
+import { NotFoundError } from '../../../shared/domain/error/not-found.error'
+import { Category } from '../../domain/entity/category.entity'
+import { CategoryInMemoryRepository } from '../../infra/db/in-memory/category-in-memory.repository'
+import { GetCategoryInput, GetCategoryUseCase } from './get-category.usecase'
+
+describe('GetCategoryUseCase Integration Tests', () => {
+  let useCase: GetCategoryUseCase
+  let repository: CategoryInMemoryRepository
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository()
+    useCase = new GetCategoryUseCase(repository)
+  })
+
+  describe('execute', () => {
+    it('should throw NotFoundError when category does not exist', async () => {
+      const input: GetCategoryInput = {
+        id: '123e4567-e89b-12d3-a456-426614174000',
+      }
+
+      await expect(useCase.execute(input)).rejects.toThrow(
+        new NotFoundError(input.id, Category)
+      )
+    })
+
+    it('should return a category successfully', async () => {
+      const category = Category.fake().aCategory().build()
+
+      await repository.insert(category)
+
+      const input: GetCategoryInput = { id: category.categoryId.id }
+      const output = await useCase.execute(input)
+
+      expect(output).toEqual({
+        id: category.categoryId.id,
+        name: category.name,
+        description: category.description,
+        isActive: category.isActive,
+        createdAt: category.createdAt,
+      })
+    })
+  })
+})
---
+++ catalog-admin-api/src/category/application/get-category/get-category.usecase.ts
@@ -0,0 +1,39 @@
+import { IUseCase } from '../../../shared/application/use-case.interface'
+import { NotFoundError } from '../../../shared/domain/error/not-found.error'
+import { Uuid } from '../../../shared/domain/value-object/value-objects/uuid.vo'
+import { Category } from '../../domain/entity/category.entity'
+import { ICategoryRepository } from '../../domain/repository/category.repository'
+
+export class GetCategoryUseCase
+  implements IUseCase<GetCategoryInput, GetCategoryOutput>
+{
+  constructor(private categoryRepo: ICategoryRepository) {}
+
+  async execute(input: GetCategoryInput): Promise<GetCategoryOutput> {
+    const uuid = new Uuid(input.id)
+    const category = await this.categoryRepo.findById(uuid)
+    if (!category) {
+      throw new NotFoundError(input.id, Category)
+    }
+
+    return {
+      id: category.categoryId.id,
+      name: category.name,
+      description: category.description,
+      isActive: category.isActive,
+      createdAt: category.createdAt,
+    }
+  }
+}
+
+export type GetCategoryInput = {
+  id: string
+}
+
+export type GetCategoryOutput = {
+  id: string
+  name: string
+  description: string
+  isActive: boolean
+  createdAt: Date
+}
