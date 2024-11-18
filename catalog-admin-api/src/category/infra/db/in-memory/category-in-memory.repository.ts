e7f260d802d44618dc0390fbcc62be7d9b47a996
Ives M. W <ivesmw@gmail.com>
2024-11-18 15:20:58 -0300

    feat: implement ISearchableRepository in ICategoryRepository

--- catalog-admin-api/src/category/infra/db/in-memory/category-in-memory.repository.ts
+++ catalog-admin-api/src/category/infra/db/in-memory/category-in-memory.repository.ts
@@ -2,11 +2,12 @@ import { SortDirection } from '../../../../shared/domain/repository/search-param
 import { Uuid } from '../../../../shared/domain/value-object/value-objects/uuid.vo'
 import { InMemorySearchableRepository } from '../../../../shared/infra/db/in-memory/in-memory.repository'
 import { Category } from '../../../domain/entity/category.entity'
+import { ICategoryRepository } from '../../../domain/repository/category.repository'
 
-export class CategoryInMemoryRepository extends InMemorySearchableRepository<
-  Category,
-  Uuid
-> {
+export class CategoryInMemoryRepository
+  extends InMemorySearchableRepository<Category, Uuid>
+  implements ICategoryRepository
+{
   sortableFields: string[] = ['name', 'created_at']
 
   protected async applyFilter(
