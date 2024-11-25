5ca4fbaf6d9b3ea135a30cc6ddc3c0654078b66e
Ives M. W <ivesmw@gmail.com>
2024-11-25 14:42:10 -0300

    feat: add database module

---
+++ catalog-admin-api/src/nest-modules/database-module/database.module.spec.ts
@@ -0,0 +1,83 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseModule } from './database.module';
+import { ConfigModule } from 'src/nest-modules/config-module/config.module';
+import { Sequelize } from 'sequelize-typescript';
+import { getConnectionToken } from '@nestjs/sequelize';
+import { ConfigService } from '@nestjs/config';
+
+describe('DatabaseModule', () => {
+  describe('sqlite', () => {
+    const connOptions = {
+      DB_VENDOR: 'sqlite',
+      DB_HOST: ':memory:',
+      DB_LOGGING: false,
+      DB_AUTOLOAD: true,
+    };
+
+    it('should create a sqlite connection', async () => {
+      const module = await Test.createTestingModule({
+        imports: [
+          DatabaseModule,
+          ConfigModule.forRoot({
+            isGlobal: true,
+            ignoreEnvFile: true,
+            ignoreEnvVars: true,
+            validationSchema: null,
+            load: [() => connOptions],
+          }),
+        ],
+      }).compile();
+      const app = module.createNestApplication();
+
+      const conn = app.get<Sequelize>(getConnectionToken());
+
+      expect(conn).toBeDefined();
+      expect(conn.options.dialect).toBe('sqlite');
+      expect(conn.options.host).toBe(':memory:');
+      expect(conn.models).toBeDefined();
+
+      await conn.close();
+    });
+  });
+
+  describe('mysql', () => {
+    const connOptions = {
+      DB_VENDOR: 'mysql',
+      DB_HOST: 'localhost',
+      DB_PORT: 3306,
+      DB_USERNAME: 'root',
+      DB_PASSWORD: 'root',
+      DB_DATABASE: 'micro_videos',
+      DB_LOGGING: false,
+      DB_AUTOLOAD: true,
+    };
+
+    it('should create a mysql connection', async () => {
+      const module = await Test.createTestingModule({
+        imports: [
+          DatabaseModule,
+          ConfigModule.forRoot({
+            isGlobal: true,
+            ignoreEnvFile: true,
+            ignoreEnvVars: true,
+            validationSchema: null,
+            load: [() => connOptions],
+          }),
+        ],
+      }).compile();
+
+      const app = module.createNestApplication();
+      const conn = app.get<Sequelize>(getConnectionToken());
+
+      expect(conn).toBeDefined();
+      expect(conn.options.dialect).toBe(connOptions.DB_VENDOR);
+      expect(conn.options.host).toBe(connOptions.DB_HOST);
+      expect(conn.options.database).toBe(connOptions.DB_DATABASE);
+      expect(conn.options.username).toBe(connOptions.DB_USERNAME);
+      expect(conn.options.password).toBe(connOptions.DB_PASSWORD);
+      expect(conn.options.port).toBe(connOptions.DB_PORT);
+
+      await conn.close();
+    });
+  });
+});
---
+++ catalog-admin-api/src/nest-modules/database-module/database.module.ts
@@ -0,0 +1,45 @@
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CONFIG_SCHEMA_TYPE } from 'src/nest-modules/config-module/config.module';
+
+const models = [CategoryModel];
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      useFactory: (configService: ConfigService<CONFIG_SCHEMA_TYPE>) => {
+        const dbVendor = configService.get('DB_VENDOR');
+
+        if (dbVendor === 'sqlite') {
+          return {
+            dialect: 'sqlite',
+            host: configService.get('DB_HOST'),
+            logging: configService.get('DB_LOGGING'),
+            autoLoadModels: configService.get('DB_AUTOLOAD'),
+            models,
+          };
+        }
+
+        if (dbVendor === 'mysql') {
+          return {
+            dialect: 'mysql',
+            host: configService.get('DB_HOST'),
+            port: configService.get('DB_PORT'),
+            username: configService.get('DB_USERNAME'),
+            password: configService.get('DB_PASSWORD'),
+            database: configService.get('DB_DATABASE'),
+            logging: configService.get('DB_LOGGING'),
+            autoLoadModels: configService.get('DB_AUTOLOAD'),
+            models,
+          };
+        }
+
+        throw new Error('Invalid DB vendor');
+      },
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
