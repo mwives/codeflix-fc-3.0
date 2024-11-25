d616737a30acd8dc0a5c56b0717dabf40a1d25be
Ives M. W <ivesmw@gmail.com>
2024-11-25 14:43:30 -0300

    feat: integrate ConfigModule and DatabaseModule into AppModule

--- catalog-admin-api/src/app.module.ts
+++ catalog-admin-api/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from './nest-modules/config-module/config.module';
+import { DatabaseModule } from './nest-modules/database-module/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
 })
 export class AppModule {}
