78a48855745106b3a85329b9efeb1ca287fad225
Ives M. W <ivesmw@gmail.com>
2024-11-25 21:56:40 -0300

    feat: add global validation and serialization pipes to the application

--- catalog-admin-api/src/main.ts
+++ catalog-admin-api/src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   await app.listen(3000);
 }
 bootstrap();
