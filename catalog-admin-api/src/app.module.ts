fa52aaf7926f581809b885a86d981b8ebda7ea4a
Ives M. W <ivesmw@gmail.com>
2024-11-25 21:57:21 -0300

    feat: add CategoriesModule and SharedModule to AppModule imports

--- catalog-admin-api/src/app.module.ts
+++ catalog-admin-api/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { CategoriesModule } from './nest-modules/categories-module/categories.module';
 import { ConfigModule } from './nest-modules/config-module/config.module';
 import { DatabaseModule } from './nest-modules/database-module/database.module';
+import { SharedModule } from './nest-modules/shared-module/shared.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    CategoriesModule,
+    SharedModule,
+  ],
 })
 export class AppModule {}
