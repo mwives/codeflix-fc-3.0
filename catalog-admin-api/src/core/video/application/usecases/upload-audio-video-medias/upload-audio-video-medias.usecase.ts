b5f9cd9bdee5d1e23b3587981386029916d5eda4
Ives M. W <ivesmw@gmail.com>
2025-01-07 22:07:03 -0300

    feat: add upload audio and video media use case

---
+++ catalog-admin-api/src/core/video/application/usecases/upload-audio-video-medias/upload-audio-video-medias.usecase.ts
@@ -0,0 +1,69 @@
+import { IStorage } from '@core/shared/application/storage.interface';
+import { IUseCase } from '@core/shared/application/use-case.interface';
+import { NotFoundError } from '@core/shared/domain/error/not-found.error';
+import { IUnitOfWork } from '@core/shared/domain/repository/unit-of-work.interface';
+import { EntityValidationError } from '@core/shared/domain/validators/validation.error';
+import { Video, VideoId } from '@core/video/domain/entity/video.entity';
+import { Trailer } from '@core/video/domain/entity/vo/trailer.vo';
+import { VideoMedia } from '@core/video/domain/entity/vo/video-media.vo';
+import { IVideoRepository } from '@core/video/domain/repository/video.repository';
+import { UploadAudioVideoMediaInput } from './upload-audio-video-medias.input';
+
+export class UploadAudioVideoMediasUseCase
+  implements IUseCase<UploadAudioVideoMediaInput, UploadAudioVideoMediaOutput>
+{
+  constructor(
+    private uow: IUnitOfWork,
+    private videoRepo: IVideoRepository,
+    private storage: IStorage,
+  ) {}
+
+  async execute(
+    input: UploadAudioVideoMediaInput,
+  ): Promise<UploadAudioVideoMediaOutput> {
+    const video = await this.videoRepo.findById(new VideoId(input.videoId));
+
+    if (!video) {
+      throw new NotFoundError(input.videoId, Video);
+    }
+
+    const audioVideoMediaMap = {
+      trailer: Trailer,
+      video: VideoMedia,
+    };
+
+    const audioMediaClass = audioVideoMediaMap[input.field] as
+      | typeof Trailer
+      | typeof VideoMedia;
+    const [audioVideoMedia, errorAudioMedia] = audioMediaClass
+      .createFromFile({
+        ...input.file,
+        videoId: video.videoId,
+      })
+      .asArray();
+
+    if (errorAudioMedia) {
+      throw new EntityValidationError([
+        {
+          [input.field]: [errorAudioMedia.message],
+        },
+      ]);
+    }
+
+    audioVideoMedia instanceof Trailer && video.replaceTrailer(audioVideoMedia);
+    audioVideoMedia instanceof VideoMedia &&
+      video.replaceVideo(audioVideoMedia);
+
+    await this.storage.store({
+      data: input.file.data,
+      id: audioVideoMedia.rawUrl,
+      mimeType: input.file.mimeType,
+    });
+
+    await this.uow.do(async () => {
+      return this.videoRepo.update(video);
+    });
+  }
+}
+
+export type UploadAudioVideoMediaOutput = void;
