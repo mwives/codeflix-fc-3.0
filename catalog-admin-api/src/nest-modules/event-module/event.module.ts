c4fd5591d6793303b99b27d68883d35eb077bdf8
Ives M. W <ivesmw@gmail.com>
2025-01-10 12:09:42 -0300

    feat: add EventModule with DomainEventMediator using @nestjs/event-emitter

---
+++ catalog-admin-api/src/nest-modules/event-module/event.module.ts
@@ -0,0 +1,18 @@
+import { DomainEventMediator } from '@core/shared/domain/events/domain-event-mediator';
+import { Module } from '@nestjs/common';
+import { EventEmitter2, EventEmitterModule } from '@nestjs/event-emitter';
+
+@Module({
+  imports: [EventEmitterModule.forRoot()],
+  providers: [
+    {
+      provide: DomainEventMediator,
+      inject: [EventEmitter2],
+      useFactory: (eventEmitter: EventEmitter2) => {
+        return new DomainEventMediator(eventEmitter);
+      },
+    },
+  ],
+  exports: [DomainEventMediator],
+})
+export class EventModule {}
