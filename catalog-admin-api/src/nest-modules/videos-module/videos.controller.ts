909750b6087b4ef08ad366f30ebe3de0b28c49da
Ives M. W <ivesmw@gmail.com>
2025-01-10 11:16:07 -0300

    feat: add endpoint for video upload

--- catalog-admin-api/src/nest-modules/videos-module/videos.controller.ts
+++ catalog-admin-api/src/nest-modules/videos-module/videos.controller.ts
@@ -1,8 +1,11 @@
 import { CreateVideoUseCase } from '@core/video/application/usecases/create-video/create-video.usecase';
 import { GetVideoUseCase } from '@core/video/application/usecases/get-video/get-video.usecase';
+import { UpdateVideoInput } from '@core/video/application/usecases/update-video/update-video.input';
 import { UpdateVideoUseCase } from '@core/video/application/usecases/update-video/update-video.usecase';
+import { UploadAudioVideoMediaInput } from '@core/video/application/usecases/upload-audio-video-medias/upload-audio-video-medias.input';
 import { UploadAudioVideoMediasUseCase } from '@core/video/application/usecases/upload-audio-video-medias/upload-audio-video-medias.usecase';
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
@@ -12,7 +15,10 @@ import {
   Patch,
   Post,
   UploadedFiles,
+  UseInterceptors,
+  ValidationPipe,
 } from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
 
@@ -47,12 +53,79 @@ export class VideosController {
   async update(
     @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
     @Body() updateVideoDto: UpdateVideoDto,
-  ) {}
+  ) {
+    const hasData = Object.keys(updateVideoDto).length > 0;
+
+    if (!hasData) return;
+
+    const data = await new ValidationPipe({
+      errorHttpStatusCode: 422,
+    }).transform(updateVideoDto, {
+      metatype: UpdateVideoDto,
+      type: 'body',
+    });
+
+    const input = new UpdateVideoInput({ id, ...data });
+    await this.updateUseCase.execute(input);
+
+    return await this.getUseCase.execute({ id });
+  }
 
   @Patch(':id/upload')
-  uploadFile(
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'banner', maxCount: 1 },
+      { name: 'thumbnail', maxCount: 1 },
+      { name: 'thumbnailHalf', maxCount: 1 },
+      { name: 'trailer', maxCount: 1 },
+      { name: 'video', maxCount: 1 },
+    ]),
+  )
+  async uploadFile(
+    @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
     @UploadedFiles()
-    @Body()
-    data,
-  ) {}
+    files: {
+      banner?: Express.Multer.File[];
+      thumbnail?: Express.Multer.File[];
+      thumbnailHalf?: Express.Multer.File[];
+      trailer?: Express.Multer.File[];
+      video?: Express.Multer.File[];
+    },
+  ) {
+    const hasMoreThanOneFile = Object.keys(files).length > 1;
+
+    if (hasMoreThanOneFile) {
+      throw new BadRequestException('Only one file can be sent');
+    }
+
+    const hasAudioVideoMedia = files.trailer?.length || files.video?.length;
+    const field = Object.keys(files)[0];
+    const file = files[field][0];
+
+    if (hasAudioVideoMedia) {
+      const dto: UploadAudioVideoMediaInput = {
+        videoId: id,
+        field: field as any,
+        file: {
+          rawName: file.originalname,
+          data: file.buffer,
+          mimeType: file.mimetype,
+          size: file.size,
+        },
+      };
+
+      const input = await new ValidationPipe({
+        errorHttpStatusCode: 422,
+      }).transform(dto, {
+        metatype: UploadAudioVideoMediaInput,
+        type: 'body',
+      });
+
+      await this.uploadAudioVideoMedia.execute(input);
+    } else {
+      //use case upload image media
+    }
+
+    return await this.getUseCase.execute({ id });
+  }
 }
