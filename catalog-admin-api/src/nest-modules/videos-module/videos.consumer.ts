de6a93706fa8cf78653693dccb54429a6cd8665c
Ives M. W <ivesmw@gmail.com>
2025-01-15 20:03:48 -0300

    feat: add video consumer to process video conversion messages from RabbitMQ

---
+++ catalog-admin-api/src/nest-modules/videos-module/videos.consumer.ts
@@ -0,0 +1,51 @@
+import { AudioVideoMediaStatus } from '@core/shared/domain/value-object/value-objects/audio-video-media.vo';
+import { ProcessAudioVideoMediasInput } from '@core/video/application/usecases/process-audio-video-medias/process-audio-video-medias.input';
+import { ProcessAudioVideoMediasUseCase } from '@core/video/application/usecases/process-audio-video-medias/process-audio-video-medias.usecase';
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
+import { Injectable, ValidationPipe } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+
+@Injectable()
+export class VideosConsumers {
+  constructor(private moduleRef: ModuleRef) {}
+
+  @RabbitSubscribe({
+    exchange: 'amq.direct',
+    routingKey: 'videos.convert',
+    queue: 'micro-videos/admin',
+    allowNonJsonMessages: true,
+  })
+  async onProcessVideo(msg: {
+    video: {
+      resource_id: string;
+      encoded_video_folder: string;
+      status: 'COMPLETED' | 'FAILED';
+    };
+  }) {
+    const resource_id = `${msg.video?.resource_id}`;
+    const [video_id, field] = resource_id.split('.');
+    const input = new ProcessAudioVideoMediasInput({
+      videoId: video_id,
+      field: field as 'trailer' | 'video',
+      encodedLocation: msg.video?.encoded_video_folder,
+      status: msg.video?.status as AudioVideoMediaStatus,
+    });
+
+    try {
+      await new ValidationPipe({
+        errorHttpStatusCode: 422,
+      }).transform(input, {
+        metatype: ProcessAudioVideoMediasInput,
+        type: 'body',
+      });
+
+      const useCase = await this.moduleRef.resolve(
+        ProcessAudioVideoMediasUseCase,
+      );
+
+      await useCase.execute(input);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
