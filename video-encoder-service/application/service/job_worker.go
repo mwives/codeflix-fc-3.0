b0ba0d01bbdb7c8ab9846467fcb05e60b99b7cb3
Ives M. W <ivesmw@gmail.com>
2025-02-11 15:29:24 -0300

    feat: add mutex for thread-safe job processing in JobWorker

--- video-encoder-service/application/service/job_worker.go
+++ video-encoder-service/application/service/job_worker.go
@@ -5,6 +5,7 @@ import (
 	"encoder/framework/utils"
 	"encoding/json"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
@@ -16,6 +17,8 @@ type JobWorkerResult struct {
 	Message *amqp.Delivery
 	Error   error
 }
+
+var Mutex = &sync.Mutex{}
 
 func JobWorker(messageChannel chan amqp.Delivery, returnChan chan JobWorkerResult, jobService JobService, job domain.Job, workerID int) {
 	for message := range messageChannel {
@@ -31,7 +34,9 @@ func JobWorker(messageChannel chan amqp.Delivery, returnChan chan JobWorkerResul
 			continue
 		}
 
+		Mutex.Lock()
 		jobService.VideoService.Video.ID = uuid.New().String()
+		Mutex.Unlock()
 
 		err = jobService.VideoService.Video.Validate()
 		if err != nil {
@@ -39,7 +44,9 @@ func JobWorker(messageChannel chan amqp.Delivery, returnChan chan JobWorkerResul
 			continue
 		}
 
+		Mutex.Lock()
 		err = jobService.VideoService.InsertVideo()
+		Mutex.Unlock()
 		if err != nil {
 			returnChan <- returnJobResult(domain.Job{}, &message, err)
 			continue
@@ -51,7 +58,9 @@ func JobWorker(messageChannel chan amqp.Delivery, returnChan chan JobWorkerResul
 		job.Status = "STARTING"
 		job.CreatedAt = time.Now()
 
+		Mutex.Lock()
 		_, err = jobService.JobRepository.Insert(&job)
+		Mutex.Unlock()
 		if err != nil {
 			returnChan <- returnJobResult(domain.Job{}, &message, err)
 			continue
