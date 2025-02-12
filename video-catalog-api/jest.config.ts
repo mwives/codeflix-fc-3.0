a8b8329fe08853cef6a7550e490ca442c3c18b64
Ives M. W <ivesmw@gmail.com>
2025-02-12 14:51:58 -0300

    feat: configure SWC for TypeScript support and update Jest configuration

---
+++ video-catalog-api/jest.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: false,
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  coverageProvider: 'v8',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'shared-module/testing',
+    'shared/testing',
+    '-fixture.ts',
+    '.d.ts',
+    '.input.ts',
+    '.interface.ts',
+    'validator-rules.ts',
+    'sequelize/migrations',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testEnvironment: 'node',
+  testRegex: '.*\\..*spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': '@swc/jest',
+  },
+};
+
+export default config;
