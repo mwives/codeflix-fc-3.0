3582c650fa9cdaaffb09311652ae4f7ccd9d87c4
Ives M. W <ivesmw@gmail.com>
2025-02-14 11:12:09 -0300

    feat: implement category domain with validation and repository structure

---
+++ video-catalog-api/src/core/category/domain/category-aggregate.validator.ts
@@ -0,0 +1,28 @@
+import { ClassValidatorFields } from '@core/shared/domain/validators/class-validator-fields';
+import { Notification } from '@core/shared/domain/validators/notification';
+import { MaxLength } from 'class-validator';
+import { Category } from './category.aggregate';
+
+export class CategoryRules {
+  @MaxLength(255, { groups: ['name'] })
+  name: string;
+
+  constructor(entity: Category) {
+    Object.assign(this, entity);
+  }
+}
+
+export class CategoryValidator extends ClassValidatorFields {
+  validate(notification: Notification, data: any, fields?: string[]): boolean {
+    const newFields = fields?.length ? fields : ['name'];
+    return super.validate(notification, new CategoryRules(data), newFields);
+  }
+}
+
+export class CategoryValidatorFactory {
+  static create() {
+    return new CategoryValidator();
+  }
+}
+
+export default CategoryValidatorFactory;
