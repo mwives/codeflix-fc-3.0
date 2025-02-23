67c7f608b7f97501602294c1648c9de48d6391e9
Ives M. W <ivesmw@gmail.com>
2025-02-23 15:10:05 -0300

    feat: implement SaveCategory use case

---
+++ video-catalog-api/src/core/category/application/use-cases/common/save-category/save-category.use-case.int.spec.ts
@@ -0,0 +1,77 @@
+import { Category, CategoryId } from '@core/category/domain/category.aggregate';
+import { CategoryElasticSearchRepository } from '@core/category/infra/db/elastic-search/category-elastic-search';
+import { setupElasticsearch } from '@core/shared/infra/testing/global-helpers';
+import { SaveCategoryInput } from './save-category.input';
+import { SaveCategoryUseCase } from './save-category.use-case';
+
+describe('SaveCategoryUseCase Integration Tests', () => {
+  let useCase: SaveCategoryUseCase;
+  let repository: CategoryElasticSearchRepository;
+
+  const esHelper = setupElasticsearch();
+
+  beforeEach(() => {
+    repository = new CategoryElasticSearchRepository(
+      esHelper.esClient,
+      esHelper.indexName,
+    );
+    useCase = new SaveCategoryUseCase(repository);
+  });
+
+  it('should create a category', async () => {
+    const uuid = '4e9e2e4e-0d1a-4a4b-8c0a-5b0e4e4e4e4e';
+    const created_at = new Date();
+
+    const output = await useCase.execute(
+      new SaveCategoryInput({
+        category_id: uuid,
+        name: 'test',
+        description: 'some description',
+        is_active: false,
+        created_at: created_at,
+      }),
+    );
+    expect(output).toStrictEqual({
+      id: uuid,
+      created: true,
+    });
+
+    const entity = await repository.findById(new CategoryId(uuid));
+    expect(entity).toMatchObject({
+      name: 'test',
+      description: 'some description',
+      is_active: false,
+      created_at,
+    });
+  });
+
+  it('should update a category', async () => {
+    const uuid = '4e9e2e4e-0d1a-4a4b-8c0a-5b0e4e4e4e4e';
+    const created_at = new Date();
+    const category = Category.fake().aCategory().build();
+
+    await repository.insert(category);
+
+    const output = await useCase.execute(
+      new SaveCategoryInput({
+        category_id: uuid,
+        name: 'test',
+        description: 'some description',
+        is_active: false,
+        created_at: created_at,
+      }),
+    );
+    expect(output).toStrictEqual({
+      id: uuid,
+      created: true,
+    });
+
+    const entity = await repository.findById(new CategoryId(uuid));
+    expect(entity).toMatchObject({
+      name: 'test',
+      description: 'some description',
+      is_active: false,
+      created_at,
+    });
+  });
+});
---
+++ video-catalog-api/src/core/category/application/use-cases/common/save-category/save-category.use-case.ts
@@ -0,0 +1,58 @@
+import { Category, CategoryId } from '@core/category/domain/category.aggregate';
+import { ICategoryRepository } from '@core/category/domain/category.repository';
+import { IUseCase } from '@core/shared/application/use-case-interface';
+import { EntityValidationError } from '@core/shared/domain/validators/validation.error';
+import { SaveCategoryInput } from './save-category.input';
+
+export type SaveCategoryOutput = { id: string; created: boolean };
+
+export class SaveCategoryUseCase
+  implements IUseCase<SaveCategoryInput, SaveCategoryOutput>
+{
+  constructor(private categoryRepository: ICategoryRepository) {}
+
+  async execute(input: SaveCategoryInput): Promise<SaveCategoryOutput> {
+    const categoryId = new CategoryId(input.category_id);
+    const category = await this.categoryRepository.findById(categoryId);
+
+    return category
+      ? this.updateCategory(input, category)
+      : this.createCategory(input);
+  }
+
+  private async createCategory(input: SaveCategoryInput) {
+    const entity = Category.create({
+      ...input,
+      category_id: new CategoryId(input.category_id),
+    });
+
+    if (entity.notification.hasErrors()) {
+      throw new EntityValidationError(entity.notification.toJSON());
+    }
+
+    await this.categoryRepository.insert(entity);
+
+    return { id: entity.category_id.id, created: true };
+  }
+
+  private async updateCategory(input: SaveCategoryInput, category: Category) {
+    category.changeName(input.name);
+    category.changeDescription(input.description);
+
+    if (input.is_active) {
+      category.activate();
+    } else {
+      category.deactivate();
+    }
+
+    category.changeCreatedAt(input.created_at);
+
+    if (category.notification.hasErrors()) {
+      throw new EntityValidationError(category.notification.toJSON());
+    }
+
+    await this.categoryRepository.update(category);
+
+    return { id: category.category_id.id, created: false };
+  }
+}
