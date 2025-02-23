5409ede63dc86d859e6b70c749fa982edc6c8458
Ives M. W <ivesmw@gmail.com>
2025-02-23 15:24:11 -0300

    feat: add CategoryOutputMapper

---
+++ video-catalog-api/src/core/category/application/use-cases/common/category-output.spec.ts
@@ -0,0 +1,27 @@
+import { Category, CategoryId } from '@core/category/domain/category.aggregate';
+import { CategoryOutputMapper } from './category-output';
+
+describe('CategoryOutputMapper Unit Tests', () => {
+  it('should convert a category in output', () => {
+    const entity = Category.create({
+      category_id: new CategoryId(),
+      name: 'any_name',
+      description: 'any_description',
+      is_active: true,
+      created_at: new Date(),
+    });
+
+    const spyToJSON = jest.spyOn(entity, 'toJSON');
+
+    const output = CategoryOutputMapper.toOutput(entity);
+
+    expect(spyToJSON).toHaveBeenCalled();
+    expect(output).toStrictEqual({
+      id: entity.category_id.id,
+      name: 'any_name',
+      description: 'any_description',
+      is_active: true,
+      created_at: entity.created_at,
+    });
+  });
+});
---
+++ video-catalog-api/src/core/category/application/use-cases/common/category-output.ts
@@ -0,0 +1,19 @@
+import { Category } from '@core/category/domain/category.aggregate';
+
+export type CategoryOutput = {
+  id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
+};
+
+export class CategoryOutputMapper {
+  static toOutput(entity: Category): CategoryOutput {
+    const { category_id, ...category } = entity.toJSON();
+    return {
+      id: category_id,
+      ...category,
+    };
+  }
+}
