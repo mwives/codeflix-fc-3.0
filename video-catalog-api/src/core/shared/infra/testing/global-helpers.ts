44c758fa84c91974f901f7c939b541a29bcf0e49
Ives M. W <ivesmw@gmail.com>
2025-02-23 14:19:34 -0300

    feat: add debug logging and improve Elasticsearch container setup in tests

--- video-catalog-api/src/core/shared/infra/testing/global-helpers.ts
+++ video-catalog-api/src/core/shared/infra/testing/global-helpers.ts
@@ -4,25 +4,57 @@ import {
   StartedElasticsearchContainer,
 } from '@testcontainers/elasticsearch';
 import crypto from 'crypto';
+import debug from 'debug';
 import { esMapping } from '../db/elastic-search/es-mapping';
 
 global.fail = (message) => {
   throw new Error(message);
 };
 
-export function setupElasticsearch() {
+export async function tryStartContainer<T>(fn: () => Promise<T>): Promise<T> {
+  do {
+    try {
+      return await fn();
+    } catch (e) {
+      if (!e.message.includes('port is already allocated')) {
+        throw e;
+      }
+    }
+    // eslint-disable-next-line no-constant-condition
+  } while (true);
+}
+
+const esDebug = debug('eshelper');
+
+type SetupElasticSearchHelper = {
+  deleteIndex: boolean;
+};
+
+export function setupElasticsearch(
+  options: SetupElasticSearchHelper = { deleteIndex: true },
+) {
   let _esClient: ElasticsearchService;
   let _startedContainer: StartedElasticsearchContainer;
   let _indexName: string;
 
   beforeAll(async () => {
-    const esContainer = new ElasticsearchContainer('elasticsearch:7.17.7');
-
-    _startedContainer = await esContainer.start();
+    _startedContainer = await tryStartContainer(async () => {
+      return new ElasticsearchContainer('elasticsearch:7.17.7')
+        .withReuse()
+        .withTmpFs({
+          '/usr/share/elasticsearch/data': 'rw',
+        })
+        .withExposedPorts({
+          container: 9200,
+          host: 9300,
+        })
+        .start();
+    });
   }, 120000);
 
   beforeEach(async () => {
     _indexName = 'test_es_' + crypto.randomInt(0, 1000000);
+    esDebug('Creating index %s', _indexName);
     _esClient = new ElasticsearchService({
       node: _startedContainer.getHttpUrl(),
     });
@@ -35,13 +67,10 @@ export function setupElasticsearch() {
   });
 
   afterEach(async () => {
-    await _esClient.indices.delete({
-      index: _indexName,
-    });
-  });
-
-  afterAll(async () => {
-    await _startedContainer.stop();
+    if (!options.deleteIndex) {
+      return;
+    }
+    await _esClient?.indices?.delete({ index: _indexName });
   });
 
   return {
