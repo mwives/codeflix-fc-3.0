a20cb0165725621ae5c5ca23f1532c572174a6ba
Ives M. W <ivesmw@gmail.com>
2025-02-23 15:24:16 -0300

    feat: implement GetCategory use case

---
+++ video-catalog-api/src/core/category/application/use-cases/common/get-category/get-category.use-case.spec.ts
@@ -0,0 +1,38 @@
+import { Category, CategoryId } from '@core/category/domain/category.aggregate';
+import { CategoryInMemoryRepository } from '@core/category/infra/db/in-memory/category-in-memory.repository';
+import { NotFoundError } from '@core/shared/domain/errors/not-found.error';
+import { GetCategoryUseCase } from './get-category.use-case';
+
+describe('GetCategoryUseCase Unit Tests', () => {
+  let useCase: GetCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new GetCategoryUseCase(repository);
+  });
+
+  it('should throws error when aggregate not found', async () => {
+    const categoryId = new CategoryId();
+    await expect(() => useCase.execute({ id: categoryId.id })).rejects.toThrow(
+      new NotFoundError(categoryId.id, Category),
+    );
+  });
+
+  it('should return a category', async () => {
+    const category = Category.fake().aCategory().build();
+    await repository.insert(category);
+
+    const categoryOutput = await useCase.execute({
+      id: category.category_id.id,
+    });
+
+    expect(categoryOutput).toEqual({
+      id: category.category_id.id,
+      name: category.name,
+      description: category.description,
+      is_active: category.is_active,
+      created_at: category.created_at,
+    });
+  });
+});
---
+++ video-catalog-api/src/core/category/application/use-cases/common/get-category/get-category.use-case.ts
@@ -0,0 +1,32 @@
+import { Category, CategoryId } from '@core/category/domain/category.aggregate';
+import { ICategoryRepository } from '@core/category/domain/category.repository';
+import { IUseCase } from '@core/shared/application/use-case-interface';
+import { NotFoundError } from '@core/shared/domain/errors/not-found.error';
+import { CategoryOutput, CategoryOutputMapper } from '../category-output';
+
+export type GetCategoryOutput = CategoryOutput;
+
+export class GetCategoryUseCase
+  implements IUseCase<GetCategoryInput, GetCategoryOutput>
+{
+  constructor(private categoryRepo: ICategoryRepository) {}
+
+  async execute(input: GetCategoryInput): Promise<GetCategoryOutput> {
+    const categoryId = new CategoryId(input.id);
+
+    const category = await this.categoryRepo.findOneBy({
+      category_id: categoryId,
+      is_active: true,
+    });
+
+    if (!category) {
+      throw new NotFoundError(input.id, Category);
+    }
+
+    return CategoryOutputMapper.toOutput(category);
+  }
+}
+
+export type GetCategoryInput = {
+  id: string;
+};
