cabc6320496fdf22bf2787329a710086342e61f6
Ives M. W <ivesmw@gmail.com>
2025-02-24 14:49:40 -0300

    feat: implement delete cast member use case

---
+++ video-catalog-api/src/core/cast-member/application/use-cases/delete-cast-member/__tests__/delete-cast-member.use-case.int.spec.ts
@@ -0,0 +1,40 @@
+import {
+  CastMember,
+  CastMemberId,
+} from '@core/cast-member/domain/cast-member.aggregate';
+import { CastMemberElasticSearchRepository } from '@core/cast-member/infra/db/elastic-search/cast-member-elastic-search';
+import { NotFoundError } from '@core/shared/domain/errors/not-found.error';
+import { setupElasticsearch } from '@core/shared/infra/testing/global-helpers';
+import { DeleteCastMemberUseCase } from '../delete-cast-member.use-case';
+
+describe('DeleteCastMemberUseCase Integration Tests', () => {
+  let useCase: DeleteCastMemberUseCase;
+  let repository: CastMemberElasticSearchRepository;
+
+  const esHelper = setupElasticsearch();
+
+  beforeEach(() => {
+    repository = new CastMemberElasticSearchRepository(
+      esHelper.esClient,
+      esHelper.indexName,
+    );
+    useCase = new DeleteCastMemberUseCase(repository);
+  });
+
+  it('should throws error when entity not found', async () => {
+    const castMemberId = new CastMemberId();
+    await expect(() => useCase.execute(castMemberId.id)).rejects.toThrow(
+      new NotFoundError(castMemberId.id, CastMember),
+    );
+  });
+
+  it('should delete a cast member', async () => {
+    const castMember = CastMember.fake().aDirector().build();
+    await repository.insert(castMember);
+    await useCase.execute(castMember.cast_member_id.id);
+    const noEntity = await repository
+      .ignoreSoftDeleted()
+      .findById(castMember.cast_member_id);
+    expect(noEntity).toBeNull();
+  });
+});
