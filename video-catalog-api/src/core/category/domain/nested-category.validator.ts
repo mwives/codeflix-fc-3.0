1ef5214f835cf52f371ea182f71ea5bc94d18e4d
Ives M. W <ivesmw@gmail.com>
2025-02-26 14:23:42 -0300

    feat: implement nested category entity and validator

---
+++ video-catalog-api/src/core/category/domain/nested-category.validator.ts
@@ -0,0 +1,30 @@
+import { ClassValidatorFields } from '@core/shared/domain/validators/class-validator-fields';
+import { Notification } from '@core/shared/domain/validators/notification';
+import { MaxLength } from 'class-validator';
+import { Category } from './category.aggregate';
+
+export class NestedCategoryRules {
+  @MaxLength(255, { groups: ['name'] })
+  name: string;
+
+  constructor(entity: Category) {
+    Object.assign(this, entity);
+  }
+}
+
+export class NestedCategoryValidator extends ClassValidatorFields {
+  validate(notification: Notification, data: any, fields?: string[]): boolean {
+    const newFields = fields?.length ? fields : ['name'];
+    return super.validate(
+      notification,
+      new NestedCategoryRules(data),
+      newFields,
+    );
+  }
+}
+
+export class NestedCategoryValidatorFactory {
+  static create() {
+    return new NestedCategoryValidator();
+  }
+}
