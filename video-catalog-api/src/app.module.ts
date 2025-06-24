4340e541928b94b79fb4b903bae8140ed18cf5ac
Ives M. W <ivesmw@gmail.com>
2025-06-24 18:10:51 -0300

    feat: add ElasticSearchModule with async configuration and global scope

--- video-catalog-api/src/app.module.ts
+++ video-catalog-api/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CastMembersModule } from './nest-modules/cast-members-module/cast-members.module';
 import { CategoriesModule } from './nest-modules/categories-module/categories.module';
+import { ConfigModule } from './nest-modules/config-module/config-module';
+import { ElasticSearchModule } from './nest-modules/elastic-search-module/elastic-search-module';
 import { GenresModule } from './nest-modules/genres-module/genres.module';
 import { VideosModule } from './nest-modules/videos-modules/videos.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    ElasticSearchModule,
     CategoriesModule.forRoot(),
     CastMembersModule.forRoot(),
     GenresModule.forRoot(),
