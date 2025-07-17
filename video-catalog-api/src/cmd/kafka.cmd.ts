1dd09e50baa02674274e97984c00299e24d286b2
Ives M. W <ivesmw@gmail.com>
2025-07-17 19:49:54 -0300

    feat: integrate Confluent Kafka with NestJS microservices

--- video-catalog-api/src/cmd/kafka.cmd.ts
+++ video-catalog-api/src/cmd/kafka.cmd.ts
@@ -1,27 +1,29 @@
 import { SchemaRegistryClient } from '@confluentinc/schemaregistry';
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
+import { ConfluentKafkaServer } from 'src/nest-modules/kafka-module/confluent/confluent-kafka-server';
 import { KConnectEventPatternRegister } from 'src/nest-modules/kafka-module/kconnect-event-pattern.register';
 import { SchemaRegistryDeserializer } from 'src/nest-modules/kafka-module/schema-registry-deserializer';
 import { AppModule } from '../app.module';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.KAFKA,
-    options: {
-      client: {
-        brokers: ['localhost:9092'],
+    strategy: new ConfluentKafkaServer({
+      server: {
+        'bootstrap.servers': 'localhost:9092',
       },
       consumer: {
-        groupId: 'categories-consumer' + Math.random(),
+        allowAutoTopicCreation: true,
+        sessionTimeout: 10000,
+        rebalanceTimeout: 10000,
       },
       deserializer: new SchemaRegistryDeserializer(
         new SchemaRegistryClient({ baseURLs: ['http://localhost:8083'] }),
       ),
-    },
+    }),
   });
 
   await app.get(KConnectEventPatternRegister).registerKConnectTopicDecorator();
+
   await app.listen();
 }
 bootstrap();
