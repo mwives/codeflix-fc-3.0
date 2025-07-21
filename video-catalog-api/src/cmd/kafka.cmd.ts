133d84c1fdad6aaa21400d4c3583706891f8f2b6
Ives M. W <ivesmw@gmail.com>
2025-07-21 14:49:33 -0300

    feat: implement retriable Kafka server with async retry capabilities and error handling

--- video-catalog-api/src/cmd/kafka.cmd.ts
+++ video-catalog-api/src/cmd/kafka.cmd.ts
@@ -1,13 +1,38 @@
 import { SchemaRegistryClient } from '@confluentinc/schemaregistry';
 import { NestFactory } from '@nestjs/core';
-import { ConfluentKafkaServer } from 'src/nest-modules/kafka-module/confluent/confluent-kafka-server';
+import { RetryTopicNaming } from 'kafkajs-async-retry';
+import { ConfluentKafkaRetriableServer } from 'src/nest-modules/kafka-module/confluent/confluent-kafka-retriable-server';
+import { KafkaConsumeErrorFilter } from 'src/nest-modules/kafka-module/kafka-consume-error.filter';
 import { KConnectEventPatternRegister } from 'src/nest-modules/kafka-module/kconnect-event-pattern.register';
 import { SchemaRegistryDeserializer } from 'src/nest-modules/kafka-module/schema-registry-deserializer';
 import { AppModule } from '../app.module';
 
 async function bootstrap() {
+  // const app = await NestFactory.createMicroservice(AppModule, {
+  //   strategy: new ConfluentKafkaServer({
+  //     server: {
+  //       'bootstrap.servers': 'localhost:9092',
+  //     },
+  //     consumer: {
+  //       allowAutoTopicCreation: true,
+  //       sessionTimeout: 10000,
+  //       rebalanceTimeout: 10000,
+  //     },
+  //     deserializer: new SchemaRegistryDeserializer(
+  //       new SchemaRegistryClient({ baseURLs: ['http://localhost:8083'] }),
+  //     ),
+  //   }),
+  // });
+
   const app = await NestFactory.createMicroservice(AppModule, {
-    strategy: new ConfluentKafkaServer({
+    strategy: new ConfluentKafkaRetriableServer({
+      asyncRetryConfig: {
+        maxRetries: 3,
+        maxWaitTime: 120000,
+        retryDelays: [1, 3, 9],
+        //retryDelays: [5, 30, 60],
+        retryTopicNaming: RetryTopicNaming.ATTEMPT_BASED,
+      },
       server: {
         'bootstrap.servers': 'localhost:9092',
       },
@@ -21,6 +46,8 @@ async function bootstrap() {
       ),
     }),
   });
+
+  app.useGlobalFilters(new KafkaConsumeErrorFilter());
 
   await app.get(KConnectEventPatternRegister).registerKConnectTopicDecorator();
 
